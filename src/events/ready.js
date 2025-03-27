e669b66174bc7f46130ad933d69a798b546c5ad2
SolareFlame <zeldac332@gmail.com>
2025-03-27 23:46:15 +0100

    👽️ Switch to node-mcstatus due to deprecated api and errors on previous module

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,8 @@
-const util = require('minecraft-server-util');
+const mcs = require('node-mcstatus');
 require('dotenv').config();
 
-const SERVER_IP = process.env.SERVER_IP;
-const SERVER_PORT =  parseInt(process.env.SERVER_PORT);
+const SERVER_IP = process.env.MC_SERVER_IP;
+const SERVER_PORT = parseInt(process.env.MC_SERVER_PORT);
 const VOICE_CHANNEL_ID = process.env.CONNECTED_VOICE_CHANNEL_ID;
 
 module.exports = (client) => {
@@ -10,30 +10,37 @@ module.exports = (client) => {
 
     const updateStatus = async () => {
         try {
-            const response = await util.status(SERVER_IP, SERVER_PORT);
-            const playerCount = response.players.online;
+            const options = { query: true };
+            const result = await mcs.statusJava(SERVER_IP, SERVER_PORT, options);
 
+            if (result.online) {
+                const playerCount = result.players.online;
 
+                console.log("Check made, players online: " + playerCount);
 
-            client.user.setPresence({
-                activities: [{ name: `Purpur-SMP avec ${playerCount} joueurs`}],
-            });
+                client.user.setPresence({
+                    activities: [{ name: `Purpur-SMP avec ${playerCount} joueurs` }],
+                });
 
-            const channel = client.channels.cache.get(VOICE_CHANNEL_ID);
-            if (channel) {
-                await channel.setName(` ${playerCount} connectés`);
+                const channel = client.channels.cache.get(VOICE_CHANNEL_ID);
+                if (channel) {
+                    await channel.setName(`${playerCount} connectés`);
+                }
+            } else {
+                client.user.setPresence({
+                    activities: [{ name: 'Serveur hors ligne', type: 'WATCHING' }],
+                    status: 'dnd'
+                });
             }
-
         } catch (error) {
             console.error('❌ Impossible de récupérer le statut du serveur:', error);
             client.user.setPresence({
-                activities: [{ name: 'Serveur hors ligne', type: 'WATCHING' }],
+                activities: [{ name: 'Serveur indisponible', type: 'WATCHING' }],
                 status: 'dnd'
             });
         }
     };
 
-    // Met à jour le statut toutes les 30 secondes
     updateStatus();
     setInterval(updateStatus, 30000);
 };
